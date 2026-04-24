15a88072097582daf610256a7bc3c63166da8539
FAL-EDU <neitor38@gmail.com>
2026-04-24 20:52:42 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-    // TUS CREDENCIALES AQUÍ
+ apiKey: "AIzaSyBYdnEmzLEV6aroL1uNBNrkWdfEGpSDHkw",
+  authDomain: "donde-compro-oficial.firebaseapp.com",
+  projectId: "donde-compro-oficial",
+  storageBucket: "donde-compro-oficial.firebasestorage.app",
+  messagingSenderId: "113447539181",
+  appId: "1:113447539181:web:6d91fc4fe96bb83d82d7e4",
+  measurementId: "G-GSQW41YN14"    
 };
 
 const app = initializeApp(firebaseConfig);
